4e1bf85f5070307bd153f958c4d18fa0e725bfd7
kellie.hall <kellie.hall@e-hps.com>
2017-05-03 15:50:50 -0400

    add skills box needs better styling/ implemented differently

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,12 +9,12 @@ export class HomeComponent implements OnInit {
     public languages: any[] = [
         { skill: 'Typescript', icon: '' },
         { skill: 'Javascript', icon: '' },
-        { skill: 'HTML', icon: 'html5' },
-        { skill: 'CSS', icon: 'css3' },
+        { skill: 'HTML', icon: 'fa-html5' },
+        { skill: 'CSS', icon: 'fa-css3' },
         { skill: 'jQuery', icon: '' }
     ];
     public devTools: any[] = [
-        { skill: 'Git', icon: 'git' },
+        { skill: 'Git', icon: 'fa-git' },
         { skill: 'Webpack', icon: '' },
         { skill: 'Grunt', icon: '' },
         { skill: 'Jasmine', icon: '' },
@@ -23,29 +23,30 @@ export class HomeComponent implements OnInit {
         { skill: 'Node.js', icon: '' }
     ];
     public os: any[] = [
-        { skill: 'Windows', icon: 'windows' },
+        { skill: 'Windows', icon: 'fa-windows' },
         { skill: 'Debian 7 & 8', icon: '' },
-        { skill: 'Ubuntu', icon: 'linux' }
+        { skill: 'Ubuntu', icon: 'fa-linux' }
     ];
     public frameworks: any[] = [
         { skill: 'Angular 2', icon: '' },
         { skill: 'Angular 1.5', icon: '' }
     ];
     public other: any[] = [
-        { skill: 'vsCode', icon: 'code' },
+        { skill: 'vsCode', icon: 'fa-code' },
         { skill: 'MongoDB', icon: '' },
         { skill: 'Postman', icon: '' },
         { skill: 'VMWare', icon: '' },
         { skill: 'PuTTY', icon: '' },
-        { skill: 'Stack Overflow', icon: 'stack-overflow' },
-        { skill: 'Bitbucket', icon: 'bitbucket' }
+        { skill: 'Stack Overflow', icon: 'fa-stack-overflow' },
+        { skill: 'Bitbucket', icon: 'fa-bitbucket' }
     ];
 
     constructor() {
         //nothing
     }
 
-    ngOnInit() {
+    ngOnInit(): void {
         //nothing
     }
+
 }
