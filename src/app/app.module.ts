0f8dd2f90fb7565d6d8063e6a64ab01418d26397
kellie.hall <kellie.hall@e-hps.com>
2017-05-03 12:42:34 -0400

    add in testing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core'
+import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { rootRouterConfig } from './app.routes';
 import { AppComponent } from './app.component';
@@ -7,7 +7,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 import { HomeComponent } from './home/home.component';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap'
 import { MaterialModule } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -22,7 +21,6 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         HttpModule,
         RouterModule.forRoot(rootRouterConfig),
         BrowserAnimationsModule,
-        // NgbModule.forRoot()
         MaterialModule.forRoot()
     ],
     providers: [
